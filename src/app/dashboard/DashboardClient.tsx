2dbf572c69337eda5489380389170de02a96949c
Aryan Kushwaha <59092136+funinkina@users.noreply.github.com>
2025-06-16 17:54:33 +0530

    Refactor button labels and improve loading indicators in DashboardClient

--- src/app/dashboard/DashboardClient.tsx
+++ src/app/dashboard/DashboardClient.tsx
@@ -259,7 +259,7 @@ export default function DashboardClient({ }: { user: User }) {
                                                                     <path fillRule="evenodd" clipRule="evenodd" d="M15 6H17V8H15V6ZM13 10V8H15V10H13ZM11 12V10H13V12H11ZM9 14V12H11V14H9ZM7 16V14H9V16H7ZM5 16H7V18H5V16ZM3 14H5V16H3V14ZM3 14H1V12H3V14ZM11 16H13V18H11V16ZM15 14V16H13V14H15ZM17 12V14H15V12H17ZM19 10V12H17V10H19ZM21 8H19V10H21V8ZM21 8H23V6H21V8Z" fill="black" />
                                                                 </svg>
                                                             )}
-                                                            {isMarkingMessageDone && messageToMarkDone?.id === msg.id ? "Resolving..." : "Mark as Done"}
+                                                            {isMarkingMessageDone && messageToMarkDone?.id === msg.id ? "Resolving..." : "Resolve"}
                                                         </button>
                                                     </li>
                                                 ))}
@@ -273,15 +273,8 @@ export default function DashboardClient({ }: { user: User }) {
                                                 onClick={() => toggleDoneSection(person.slug)}
                                                 className="flex items-center gap-2 text-gray-600 hover:text-gray-900 font-lexend"
                                             >
-                                                <svg
-                                                    width="20"
-                                                    height="20"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className={`transform transition-transform ${isDoneSectionExpanded ? 'rotate-90' : ''}`}
-                                                >
-                                                    <path d="M8 4L16 12L8 20" stroke="currentColor" strokeWidth="2" />
+                                                <svg width="24" height="24" className={`transform transition-transform ${isDoneSectionExpanded ? 'rotate-90' : ''}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8 5L8 7L10 7L10 5L8 5ZM12 9L12 7L10 7L10 9L12 9ZM14 11L14 9L12 9L12 11L14 11ZM14 13L16 13L16 11L14 11L14 13ZM12 15L12 13L14 13L14 15L12 15ZM12 15L10 15L10 17L12 17L12 15ZM8 19L8 17L10 17L10 19L8 19Z" fill="black" />
                                                 </svg>
                                                 <span>Resolved Issues ({doneMessages.length})</span>
                                             </button>
@@ -377,9 +370,13 @@ export default function DashboardClient({ }: { user: User }) {
                                     className="px-4 py-2 bg-red-200 border-2 border-black cursor-pointer flex items-center gap-2 shadow-[5px_5px_0px_0px_rgba(0,0,0)] font-lexend text-xl font-medium hover:shadow-[10px_10px_0px_0px_rgba(0,0,0)] transition duration-200"
                                     disabled={isDeletingPerson}
                                 >
-                                    {isDeletingPerson && (
+                                    {isDeletingPerson ? (
                                         <svg width="24" height="24" className='animate-spin' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M12.9999 2H10.9999V8H12.9999V2ZM12.9999 16H10.9999V22H12.9999V16ZM21.9998 11V13L15.9998 13V11H21.9998ZM7.99963 13V11H1.99963V13L7.99963 13ZM14.9996 6.99997H16.9996V8.99997H14.9996V6.99997ZM18.9995 4.99997H16.9995V6.99997H18.9995V4.99997ZM8.99963 6.99997H6.99963V8.99997H8.99963V6.99997ZM4.99973 4.99997H6.99973V6.99997H4.99973V4.99997ZM14.9996 17H16.9995V18.9999H18.9995V16.9999H16.9996V15H14.9996V17ZM6.99963 16.9999V15H8.99963V17H6.99973V18.9999H4.99973V16.9999H6.99963Z" fill="black" />
+                                        </svg>
+                                    ) : (
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M5 3H3V21H5H19H21V3H19H5ZM19 5V19H5V5H19ZM16 11H8V13H16V11Z" fill="black" />
                                         </svg>
                                     )}
                                     {isDeletingPerson ? "Removing..." : "Remove"}
@@ -415,9 +412,13 @@ export default function DashboardClient({ }: { user: User }) {
                                     className="px-4 py-2 bg-red-200 border-2 border-black cursor-pointer flex items-center gap-2 shadow-[5px_5px_0px_0px_rgba(0,0,0)] font-lexend text-xl font-medium hover:shadow-[10px_10px_0px_0px_rgba(0,0,0)] transition duration-200"
                                     disabled={isMarkingMessageDone}
                                 >
-                                    {isMarkingMessageDone && (
+                                    {isMarkingMessageDone ? (
                                         <svg width="24" height="24" className='animate-spin' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M12.9999 2H10.9999V8H12.9999V2ZM12.9999 16H10.9999V22H12.9999V16ZM21.9998 11V13L15.9998 13V11H21.9998ZM7.99963 13V11H1.99963V13L7.99963 13ZM14.9996 6.99997H16.9996V8.99997H14.9996V6.99997ZM18.9995 4.99997H16.9995V6.99997H18.9995V4.99997ZM8.99963 6.99997H6.99963V8.99997H8.99963V6.99997ZM4.99973 4.99997H6.99973V6.99997H4.99973V4.99997ZM14.9996 17H16.9995V18.9999H18.9995V16.9999H16.9996V15H14.9996V17ZM6.99963 16.9999V15H8.99963V17H6.99973V18.9999H4.99973V16.9999H6.99963Z" fill="black" />
+                                        </svg>
+                                    ) : (
+                                        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M15 6H17V8H15V6ZM13 10V8H15V10H13ZM11 12V10H13V12H11ZM9 14V12H11V14H9ZM7 16V14H9V16H7ZM5 16H7V18H5V16ZM3 14H5V16H3V14ZM3 14H1V12H3V14ZM11 16H13V18H11V16ZM15 14V16H13V14H15ZM17 12V14H15V12H17ZM19 10V12H17V10H19ZM21 8H19V10H21V8ZM21 8H23V6H21V8Z" fill="black" />
                                         </svg>
                                     )}
                                     {isMarkingMessageDone ? "Resolving..." : "Mark as Resolved"}
